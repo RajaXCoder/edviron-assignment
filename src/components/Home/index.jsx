b623e27691eafd5f0346df8bae99cdf1b837fa94
RajaXCoder <rajasekarrdx35@gmail.com>
2025-01-12 16:47:55 +0530

    update

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -29,12 +29,15 @@ const Home = () => {
         setApiStatus(apiConst.inProgress); // Set loading state
 
         const jwtToken = Cookies.get("jwt_token");
-        const res = await axios.get("http://localhost:3000/transactions/", {
-          headers: {
-            Authorization: `Bearer ${jwtToken}`,
-            "Content-Type": "application/json",
-          },
-        });
+        const res = await axios.get(
+          "https://raja-edviron-server.onrender.com/transactions",
+          {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         const data = res.data;
         setApiStatus(apiConst.success); // Set success state on successful API response
